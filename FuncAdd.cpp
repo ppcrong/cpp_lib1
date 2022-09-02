b0307abf736179f02627c7cbc07bd3951ce2c722
ppcrong <ppcrong@gmail.com>
2022-09-02 14:37:02 +0800

    add class FuncAdd

---
+++ FuncAdd.cpp
@@ -0,0 +1,14 @@
+#include "FuncAdd.h"
+
+FuncAdd::FuncAdd()
+{
+}
+
+FuncAdd::~FuncAdd()
+{
+}
+
+int32_t FuncAdd::add(int32_t a, int32_t b)
+{
+    return a + b;
+}
---
+++ include/FuncAdd.h
@@ -0,0 +1,18 @@
+#ifndef FUNCADD_H
+#define FUNCADD_H
+
+#pragma once
+
+#include <stdint.h>
+
+class FuncAdd
+{
+public:
+    FuncAdd();
+    ~FuncAdd();
+    int32_t add(int32_t a, int32_t b);
+
+private:
+};
+
+#endif
